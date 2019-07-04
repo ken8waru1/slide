ebe6537cd0f662923cdbe2a4ca7b674caa90fd9f
ken8waru1 <ken8waru1@gmail.com>
2019-07-03 19:11:15 -0700

    Add more styling to signup and login pages, and implement demo login

--- frontend/components/session/session_form.jsx
+++ frontend/components/session/session_form.jsx
@@ -61,30 +61,39 @@ class SessionForm extends React.Component {
     //   this.props.processForm(this.state)
   }
 
+
   render () {
     return (
       <div className="session-form-container">
+        {this.props.errors.length > 0 ?  
+          <div className="errors">
+            <span className="color-bar"></span>
+            <ul>
+              {this.props.errors.map(error => (
+                <li>{error}</li>
+              ))}
+            </ul>
+          </div> : <></>
+        }
+
         <div className="session-form">
-          {this.props.errors.map(error => (
-            <p>{error}</p>
-          ))}
           <span className="session-description">{this.props.formType === 'Signup' ? "Slide into Everyone's DMs" : "Sign in to Slide" }</span>
           {this.props.link}
           <form className="session-flex" /*onSubmit={this.handleClick}*/>
             <label>
               <h3 className="session-field">Email</h3>
-              <input className="session-input" type="text" onChange={this.updateField('email')} value={this.state.email} placeholder="Email"/>
+              <input className="session-input" autoComplete="off" type="text" onChange={this.updateField('email')} value={this.state.email} placeholder="Email"/>
             </label>
             <br/>
             <label>
               <h3 className="session-field">Password</h3>
-              <input className="session-input" type="password" onChange={this.updateField('password')} value={this.state.password} placeholder="Password" />
+              <input className="session-input" autoComplete="off" type="password" onChange={this.updateField('password')} value={this.state.password} placeholder="Password" />
             </label>
             <br/>
             {this.props.formType === 'Signup' ?
               <label>
                 <h3 className="session-field">Display Name</h3>
-                <input className="session-input" type="text" onChange={this.updateField('display_name')} value={this.state.displayName} placeholder="Display Name"/>
+                <input className="session-input" autoComplete="off" type="text" onChange={this.updateField('display_name')} value={this.state.displayName} placeholder="Display Name"/>
                 <br/>
               </label>
               : <></>
