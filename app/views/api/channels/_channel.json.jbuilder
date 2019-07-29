e52c6609e6f0400672739a820e38e0d59ea13e3d
ken8waru1 <ken8waru1@gmail.com>
2019-07-29 14:26:34 -0700

    Refactor users controller

--- app/views/api/channels/_channel.json.jbuilder
+++ app/views/api/channels/_channel.json.jbuilder
@@ -1,2 +1,2 @@
-json.extract! channel, :id, :name, :purpose, :creator_id
+json.extract! channel, :id, :name, :purpose, :creator_id, :is_direct_message
 json.subscribe_count channel.subscriptions.length
--- frontend/components/channel/channel_index.jsx
+++ frontend/components/channel/channel_index.jsx
@@ -13,6 +13,8 @@ class ChannelIndex extends React.Component {
 
   render () {
     const { channels, currentUser } = this.props;
+    const publicChannels = channels.filter(channel => !channel.isDirectMessage);
+    const directMessages = channels.filter(channel => channel.isDirectMessage);
     const currentChannelId = this.props.match.params.channelId;
     return (
       <div className="chat-container">
@@ -30,7 +32,12 @@ class ChannelIndex extends React.Component {
           </div>
           <div className="channel-list">
             <div className="channel-header">Channels<img className="plus-circle" onClick={() => this.props.openModal('createChannel')} src={window.images.pluscircle} /></div>
-            {channels.map(channel => <ChannelIndexItem key={channel.id} channel={channel} currentChannel={currentChannelId} />)}
+            {publicChannels.map(channel => <ChannelIndexItem key={channel.id} channel={channel} currentChannel={currentChannelId} />)}
+          </div>
+
+          <div className="dm-list">
+            <div className="channel-header">Direct Messages<img className="plus-circle" onClick={() => this.props.openModal('createChannel')} src={window.images.pluscircle} /></div>
+            {directMessages.map(channel => <ChannelIndexItem key={channel.id} channel={channel} currentChannel={currentChannelId} />)}
           </div>
         </div>
         <div className="chat-box">
